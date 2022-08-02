48f60f5239b66332765c9694b06f4461912c9313
Aleksandr-Shevch <waidyxa@gmail.com>
2022-08-02 15:33:44 +0300

    Update languagy in workSpace Added exception

--- src/workSpace.java
+++ src/workSpace.java
@@ -1,45 +1,51 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class workSpace {
-    public static java.util.Scanner result() {
+    public static java.util.Scanner result()throws InputMismatchException {
         double num1;
         double num2;
         double answer;
         char a;
 
-        java.util.Scanner reader = new java.util.Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        num1 = reader.nextDouble();
-        System.out.print("Введите математический знак: ");
-        a = reader.next().charAt(0);
-        System.out.print("Введите второе число число: ");
-        num2 = reader.nextDouble();
-        switch (a) {
-            case '+':
-                answer = num1 + num2;
-                System.out.println(num1 + "+" + num2 + " = " + answer);
-                break;
-            case '-':
-                answer = num1 - num2;
-                System.out.println(num1 + "-" + num2 + " = " + answer);
-                break;
-            case '*':
-                answer = num1 * num2;
-                System.out.println(num1 + "*" + num2 + " = " + answer);
-                break;
-            case '/':
-                answer = num1 / num2;
-                if (num2 == 0) {
-                    System.out.println("дебил, делить на 0 нельзя");
+        try {
+            java.util.Scanner reader = new java.util.Scanner(System.in);
+            System.out.print("Enter first number: ");
+            num1 = reader.nextDouble();
+            System.out.print("Enter mathematical sign: ");
+            a = reader.next().charAt(0);
+            System.out.print("Enter second number: ");
+            num2 = reader.nextDouble();
+            switch (a) {
+                case '+':
+                    answer = num1 + num2;
+                    System.out.println(num1 + "+" + num2 + " = " + answer);
                     break;
-                }
-                System.out.println(num1 + "/" + num2 + " = " + answer);
-            default:
-                System.out.println("error");
-                break;
+                case '-':
+                    answer = num1 - num2;
+                    System.out.println(num1 + "-" + num2 + " = " + answer);
+                    break;
+                case '*':
+                    answer = num1 * num2;
+                    System.out.println(num1 + "*" + num2 + " = " + answer);
+                    break;
+                case '/':
+                    answer = num1 / num2;
+                    if (num2 == 0) {
+                        System.out.println("can't divide by zero");
+                        break;
+                    }
+                    System.out.println(num1 + "/" + num2 + " = " + answer);
+                default:
+                    System.out.println("error");
+                    break;
+            }
         }
-
-        System.out.println("Подсчёт закончен, welldone!");
+        catch(InputMismatchException e){
+                System.out.println("Numbers only!  ");
+            }
+        System.out.println("Finishing!");
+        System.out.println("You can repeat operation below: ");
         return result();
+        }
     }
-}
